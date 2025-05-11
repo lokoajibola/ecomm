7f0c916b11b6114d0489cc5d2f61d9c2ac15d93d
lokoajibola <loko.ajibola@gmail.com>
2025-05-11 23:45:04 +0100

    first commit

---
+++ tests/test_ecommerce.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+# from ..pages.login_page import LoginPage
+from pages.login_page import LoginPage  # Assuming your package is named 'ecommerce'
+# from pages.inventory_page import InventoryPage
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    yield driver
+    driver.quit()
+
+def test_ecommerce_flow(driver):
+    # Login
+    login_page = LoginPage(driver)
+    login_page.login("standard_user", "secret_sauce")
+    assert driver.find_element(By.CLASS_NAME, "title").text == "Products"
