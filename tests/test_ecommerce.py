97c487be500ed8359c4b94519e334292a114bb5a
Ajibola Loko <93357294+lokoajibola@users.noreply.github.com>
2025-05-12 10:02:16 +0100

    Update test_ecommerce.py

--- tests/test_ecommerce.py
+++ tests/test_ecommerce.py
@@ -4,16 +4,23 @@ from selenium import webdriver
 from pages.login_page import LoginPage  # Assuming your package is named 'ecommerce'
 # from pages.inventory_page import InventoryPage
 from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Automatically download and install matching chromedriver
 
 
 @pytest.fixture
 def driver():
+    chromedriver_autoinstaller.install()
+
     options = Options()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.saucedemo.com/")
     yield driver
     driver.quit()
